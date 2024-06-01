4177ecffe922482bca1c1d5b4674e7cd45258d63
StarTux <ths.loy@gmail.com>
2024-06-01 13:12:55 +0200

    1.20.6

--- src/main/java/com/winthier/tinfoil/TinfoilPlugin.java
+++ src/main/java/com/winthier/tinfoil/TinfoilPlugin.java
@@ -1,10 +1,12 @@
 package com.winthier.tinfoil;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.NamedTextColor.RED;
+import static net.kyori.adventure.text.format.NamedTextColor.YELLOW;
 
 public final class TinfoilPlugin extends JavaPlugin {
     @Override
@@ -13,7 +15,7 @@ public final class TinfoilPlugin extends JavaPlugin {
         if (args.length < 2) return false;
         final Player player = getServer().getPlayer(args[0]);
         if (player == null) {
-            sender.sendMessage("" + ChatColor.RED + "Player not found: " + args[0]);
+            sender.sendMessage(text("Player not found: " + args[0], RED));
             return true;
         }
         StringBuilder sb = new StringBuilder(args[1]);
@@ -24,10 +26,8 @@ public final class TinfoilPlugin extends JavaPlugin {
         getServer().getScheduler().runTask(this, () -> {
                 player.performCommand(commandLine);
             });
-        getLogger().info(sender.getName() + " made " + player.getName()
-                         + " type: /" + commandLine);
-        sender.sendMessage(ChatColor.YELLOW + "Made " + player.getName()
-                           + " type: /" + commandLine);
+        getLogger().info(sender.getName() + " made " + player.getName() + " type: /" + commandLine);
+        sender.sendMessage(text("Made " + player.getName() + " type: /" + commandLine, YELLOW));
         return true;
     }
 }
